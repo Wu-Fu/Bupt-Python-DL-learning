6099c963e6e7f22749980f3a2b75dae8551edb2a
Wu-Fu <1774246905@qq.com>
2022-06-30 08:27:00 +0800

    Initial commit

--- code/test.py
+++ code/test.py
@@ -1,9 +1,27 @@
+import matplotlib_inline.backend_inline
 import torch
-x = torch.tensor(3)
-if torch.cuda.is_available():
-    device = torch.device("cuda")          # GPU
-    y = torch.ones_like(x, device=device)  # 直接创建一个在GPU上的Tensor
-    x = x.to(device)                       # 等价于 .to("cuda")
-    z = x + y
-    print(z)
-    print(z.to("cpu", torch.double))       # to()还可以同时更改数据类型
+from IPython import display
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+
+num_inputs = 2
+num_examples = 1000
+true_w = [2, -3.4]
+true_b = 4.2
+features = torch.randn(num_examples, num_inputs,
+                       dtype=torch.float32)
+labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()),
+                       dtype=torch.float32)
+print(features[0],labels[0])
+
+def use_svg_display():
+    matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+set_figsize()
+plt.scatter(features[:, 1].numpy(), labels.numpy(), 1);
